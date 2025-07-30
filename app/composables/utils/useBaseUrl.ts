c7ec5b9ea73875d666cbbc2404749fe66b0084c9
Kiwi2333 <1329634286@qq.com>
2025-07-31 00:05:44 +0800

    build(env): 更新环境变量配置和构建脚本
    - 移除了 useBaseUrl 中的冗余环境变量定义
    - 更新了 package.json 中的构建和开发脚本
    - 优化了环境变量的加载顺序和方式

--- app/composables/utils/useBaseUrl.ts
+++ app/composables/utils/useBaseUrl.ts
@@ -2,12 +2,6 @@
 export interface EnvConfigMap {
   VITE_API_BASE_URL: string;
   VITE_API_WS_BASE_URL: string;
-  VITE_BASE_OSS_PATH: string;
-  VITE_XUN_FEI_WSS_URL: string;
-  VITE_XUN_FEI_APP_ID: string;
-  VITE_TURN_SERVER_URL?: string;
-  VITE_TURN_SERVER_USER?: string;
-  VITE_TURN_SERVER_PWD?: string;
 }
 
 
@@ -15,13 +9,8 @@ export interface EnvConfigMap {
 export const DefaultEnvConfigMap: EnvConfigMap = {
   VITE_API_BASE_URL: import.meta.env.VITE_API_BASE_URL,
   VITE_API_WS_BASE_URL: import.meta.env.VITE_API_WS_BASE_URL,
-  VITE_BASE_OSS_PATH: import.meta.env.VITE_BASE_OSS_PATH,
-  VITE_XUN_FEI_WSS_URL: import.meta.env.VITE_XUN_FEI_WSS_URL,
-  VITE_XUN_FEI_APP_ID: import.meta.env.VITE_XUN_FEI_APP_ID,
-  VITE_TURN_SERVER_URL: import.meta.env.VITE_TURN_SERVER_URL,
-  VITE_TURN_SERVER_USER: import.meta.env.VITE_TURN_SERVER_USER,
-  VITE_TURN_SERVER_PWD: import.meta.env.VITE_TURN_SERVER_PWD,
 };
+
 const savedConfig = useLocalStorage("env-config", DefaultEnvConfigMap, {
   mergeDefaults: true,
 });
@@ -40,7 +29,7 @@ export const BaseUrlFont = `${savedConfig.value.VITE_API_BASE_URL}/public/font`;
 export const BaseUrlRef = computed(() => savedConfig.value.VITE_API_BASE_URL);
 
 // 图片
-export const BASE_OSS_PATH = savedConfig.value.VITE_BASE_OSS_PATH;
+export const BASE_OSS_PATH = import.meta.env.VITE_BASE_OSS_PATH;
 export const BaseUrlAppFile = BASE_OSS_PATH;
 export const BaseUrlImg = BASE_OSS_PATH;
 export const BaseUrlVideo = BASE_OSS_PATH;
@@ -52,10 +41,11 @@ export const AuthKey = "Authorization";
 export const BaseWSUrl = savedConfig.value.VITE_API_WS_BASE_URL;
 export const BaseWSUrlRef = computed(() => savedConfig.value.VITE_API_WS_BASE_URL);
 
-export const XUN_FEI_WSS_URL = savedConfig.value.VITE_XUN_FEI_WSS_URL;
-export const XUN_FEI_APP_ID = savedConfig.value.VITE_XUN_FEI_APP_ID;
+export const XUN_FEI_WSS_URL = import.meta.env.VITE_XUN_FEI_WSS_URL;
+export const XUN_FEI_APP_ID = import.meta.env.VITE_XUN_FEI_APP_ID;
 
 // TURN服务器配置
-export const TURN_SERVER_URL = savedConfig.value.VITE_TURN_SERVER_URL;
-export const TURN_SERVER_USER = savedConfig.value.VITE_TURN_SERVER_USER;
-export const TURN_SERVER_PWD = savedConfig.value.VITE_TURN_SERVER_PWD;
+export const TURN_SERVER_URL = import.meta.env.VITE_TURN_SERVER_URL;
+export const TURN_SERVER_USER = import.meta.env.VITE_TURN_SERVER_USER;
+export const TURN_SERVER_PWD = import.meta.env.VITE_TURN_SERVER_PWD;
+console.log(DefaultEnvConfigMap, TURN_SERVER_URL, XUN_FEI_WSS_URL, BaseWSUrl);
