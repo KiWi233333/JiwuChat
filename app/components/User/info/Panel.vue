4ce5ff9b6f38ebfbe8d478f1ac48e5103e94cbef
KiWi233333 <1329634286@qq.com>
2025-08-30 01:55:27 +0800

    style(components): 优化用户资料面板的样式
    - 为性别、年龄和生日信息添加右侧边框，提升可读性
    - 统一边框样式使用 border-default-r 类，保持一致性

--- app/components/User/info/Panel.vue
+++ app/components/User/info/Panel.vue
@@ -345,14 +345,14 @@ onMounted(() => {
   <div class="detail-info">
     <p class="user-props truncate text-sm">
       <i mr-3 inline-block h-4 w-4 :class="user.gender === Gender.BOY ? 'i-tabler:gender-male text-blue' : user.gender === Gender.GIRL ? 'i-tabler:gender-female text-pink' : 'i-tabler:gender-transgender text-yellow'" />
-      <span class="mr-2 pr-2">
+      <span class="mr-2 border-default-r pr-2">
         {{ user.gender }}
       </span>
       <template v-if="user.birthday">
-        <span class="mr-2 border-default-l pr-2">
+        <span class="mr-2 border-default-r pr-2">
           {{ getAgeText }}
         </span>
-        <span class="mr-2 border-default-l pr-2">
+        <span class="mr-2 border-default-r pr-2">
           {{ user.birthday || ' - ' }}
         </span>
         <span>
