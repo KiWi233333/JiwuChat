9bfb6d905309985440ba42a10ef208fb9f753973
KiWi233333 <1329634286@qq.com>
2025-09-01 01:20:36 +0800

    fix(group): 修复好友页面退出群组错误

--- app/components/Chat/Friend/MainType/Group.vue
+++ app/components/Chat/Friend/MainType/Group.vue
@@ -117,7 +117,7 @@ const getRoleText = computed(() => room.value.role !== undefined ? chatRoomRoleT
         style="transition: .2s; max-width: 9em;text-align: center;letter-spacing: 1px;--el-color-primary: var(--el-color-danger);"
         plain
         class="mr-4 op-60 hover:op-100"
-        @click="chat.exitGroupConfirm(chat.theRoomId, isGroupOwner, () => {
+        @click="chat.exitGroupConfirm(roomId, isGroupOwner, () => {
           chat.setTheFriendOpt(FriendOptType.Empty, {});
           chat.setDelGroupId(roomId);
         })"
